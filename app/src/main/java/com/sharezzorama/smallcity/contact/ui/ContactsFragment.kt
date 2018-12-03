da2cf1223ba00db106430a47d32aab226ebbeac8
sharezzorama <sharezzorama@gmail.com>
2018-12-03 08:12:24 +0700

    Add contact fragment

--- app/src/main/java/com/sharezzorama/smallcity/contact/ui/ContactsFragment.kt
+++ app/src/main/java/com/sharezzorama/smallcity/contact/ui/ContactsFragment.kt
@@ -1,8 +1,14 @@
 package com.sharezzorama.smallcity.contact.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.sharezzorama.smallcity.base.BaseFragment
 
 class ContactsFragment : BaseFragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+    }
 }
